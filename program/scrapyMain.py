b18502a2c2f9f0fbedcb84e913bb293460de1ea5
zhangzhiyong111 <zzyfuzhou@163.com>
2017-04-28 22:20:33 +0800

    revise the scrapyMain.py

--- program/scrapyMain.py
+++ program/scrapyMain.py
@@ -118,3 +118,32 @@ if __name__ == '__main__' :
 # urllib.request.install_opener(opener)
 # print(urllib.request.urlopen(url).read())
 # 若IP是有效的，则可打印出网页源码，否则会出现错误。所以我们可以通过以上代码对所抓取的代理IP逐个进行验证。
+#from threading import Thread
+#from Queue import Queue
+#from time import sleep
+## q是任务队列
+##NUM是并发线程总数
+##JOBS是有多少任务
+#q = Queue()
+#NUM = 2
+#JOBS = 10
+##具体的处理函数，负责处理单个任务
+#def do_somthing_using(arguments):
+#    print arguments
+##这个是工作进程，负责不断从队列取数据并处理
+#def working():
+#    while True:
+#        arguments = q.get()
+#        do_somthing_using(arguments)
+#        sleep(1)
+#        q.task_done()
+##fork NUM个线程等待队列
+#for i in range(NUM):
+#    t = Thread(target=working)
+#    t.setDaemon(True)
+#    t.start()
+##把JOBS排入队列
+#for i in range(JOBS):
+#    q.put(i)
+##等待所有JOBS完成
+#q.join()
